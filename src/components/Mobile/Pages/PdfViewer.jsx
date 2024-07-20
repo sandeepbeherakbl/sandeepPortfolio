a7eea457006a4fdfbc8ca1f9d07b63338e22c67d
sandeepbeherakbl <sandeepbeherakbl@gmail.com>
2024-07-21 01:06:27 +0530

    pdf screen updated

--- src/components/Mobile/Pages/PdfViewer.jsx
+++ src/components/Mobile/Pages/PdfViewer.jsx
@@ -3,37 +3,66 @@ import { useState } from "react";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "../Styles/Mobile.css";
+import { StepBack, StepForward } from "lucide-react";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PdfViewer = () => {
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [error, setError] = useState(null);
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   };
 
+  const onDocumentLoadError = (error) => {
+    setError(error.message);
+  };
+
+  const goToNextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
   return (
     <div className="mob-body-container-height">
-      <div className="pdf-viewer-container">
-        <Document
-          file="/cv.pdf"
-          onLoadSuccess={onDocumentLoadSuccess}
-          className="pdf-document"
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p className="page-info">
-          Page {pageNumber} of {numPages}
-        </p>
-        <div className="pdf-download-button">
-          <a href="/cv.pdf" download>
-            <button>Download PDF</button>
-          </a>
-        </div>
+    <div className="pdf-viewer-container">
+      {error && <p>Error: {error}</p>}
+      <Document
+        file="/cv.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+        onLoadError={onDocumentLoadError}
+        className="pdf-document"
+        
+      >
+        <Page pageNumber={pageNumber} width={350} canvasBackground="transparent"  />
+      </Document>
+      <p className="page-info">
+        Page {pageNumber} of {numPages}
+      </p>
+      <div className="pdf-navigation">
+        <button onClick={goToPreviousPage} disabled={pageNumber <= 1}>
+          <StepBack/>
+        </button>
+        <button onClick={goToNextPage} disabled={pageNumber >= numPages}>
+          <StepForward/>
+        </button>
+      </div>
+      <div className="download-button">
+        <a href="/cv.pdf" download>
+          <button>Download PDF</button>
+        </a>
       </div>
     </div>
+  </div>
   );
 };
 
