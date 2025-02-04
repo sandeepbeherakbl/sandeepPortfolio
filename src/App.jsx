47c2a8861bc087611d52cda531ea55b02bb1269d
sandeep <>
2025-02-04 13:08:40 +0530

    cursor updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import SplashScreen from "./components/Desktop/pages/SplashScreen";
+import CustomCursor from "./components/Desktop/components/CustomCursor";
 
 function App() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -23,15 +24,18 @@ function App() {
   }, []);
 
   return (
-    <AnimatePresence mode="wait">
-      {isLoading ? (
-        <SplashScreen onLoadingComplete={handleLoadingComplete} />
-      ) : (
-        <Router>
-          {isMobile ? <MobileMain /> : <DesktopMain />}
-        </Router>
-      )}
-    </AnimatePresence>
+    <>
+      <CustomCursor />
+      <AnimatePresence mode="wait">
+        {isLoading ? (
+          <SplashScreen onLoadingComplete={handleLoadingComplete} />
+        ) : (
+          <Router>
+            {isMobile ? <MobileMain /> : <DesktopMain />}
+          </Router>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
 
